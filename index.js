45264902c3b8b468463919b61d781dab6b9f50c5
Razi Alfarabi <alfarabirazi@gmail.com>
2022-06-04 16:22:04 +0700

    Latihan JS Callback - Razi Alfarabi

--- index.js
+++ index.js
@@ -6,20 +6,38 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (param1, param2) => {
+  const dataNama = [];
+  if (param1 === null || param2 === undefined) {
+    for (counter = 0; counter < names.length; counter++) {
+      dataNama.push(counter + 1 + '. ' + names.sort()[counter]);
+    }
+  } else {
+    for (counter = 0; counter < param1.length; counter++) {
+      // dataNama.push(param2(param1)[counter]);
+      dataNama.push(counter + 1 + '. ' + param2(param1)[counter]);
+    }
+  }
+  return dataNama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (param1) => {
+  return param1.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (param1) => {
+  return param1.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+  // console.log(sorter?.());
+  console.log(sorter?.(names, sortAscending)?.join('\n'));
+  console.log(sorter?.(names, sortDescending)?.join('\n'));
 })();
 
 module.exports = {
